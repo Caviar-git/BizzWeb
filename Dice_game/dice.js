bf4e04daa8ad55a8d185c990aca8d079c602a1ef
Biswadeep <sarkar.biswadeep07@gmail.com>
2020-06-11 03:23:26 +0530

    Added dice game folder

---
+++ Dice_game/dice.js
@@ -0,0 +1,16 @@
+var num1 = Math.ceil(Math.random()*6);
+var num2 = Math.ceil(Math.random()*6);
+var randomPic_1='images/dice'+num1+'.png';
+var randomPic_2='images/dice'+num2+'.png';
+
+document.getElementById('dice1').setAttribute('src',randomPic_1);
+
+document.getElementById('dice2').setAttribute('src',randomPic_2);
+
+if(num1>num2){
+  document.getElementsByClassName('title')[0].textContent='Player1 Wins!!';
+}
+
+else{
+  document.getElementsByClassName('title')[0].textContent='Player2 Wins!!';
+}
