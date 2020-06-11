5972fea789ac8c062dd60c4b7ccf137481fc64af
Biswadeep <sarkar.biswadeep07@gmail.com>
2020-06-11 13:01:30 +0530

    bleh

--- Dice_game/dice.js
+++ Dice_game/dice.js
@@ -7,10 +7,12 @@ document.getElementById('dice1').setAttribute('src',randomPic_1);
 
 document.getElementById('dice2').setAttribute('src',randomPic_2);
 
-if(num1>num2){
+if(num1>num2)
   document.getElementsByClassName('title')[0].textContent='Player1 Wins!!';
-}
 
-else{
+
+else if(num2>num1)
   document.getElementsByClassName('title')[0].textContent='Player2 Wins!!';
-}
+
+else
+  document.getElementsByClassName('title')[0].textContent='Draw';
